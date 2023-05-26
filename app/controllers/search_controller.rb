fcfafb9cd039f5a7ff42d06556b5de1fe5b88853
Andrew Bingham <andrewgbingham@gmail.com>
2023-05-26 13:38:04 -0600

    feat: nation member count

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,12 @@
 class SearchController < ApplicationController 
   def index
+    search_params = params[:nation]
     
+    conn = Faraday.new(url: "https://last-airbender-api.fly.dev/")
+    
+    response = conn.get("/api/v1/characters?affiliation=#{search_params}&perPage=100")
+    
+    @members = JSON.parse(response.body, symbolize_names: true)
+    # require 'pry'; binding.pry
   end
 end
